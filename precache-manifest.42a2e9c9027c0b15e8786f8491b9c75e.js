58e9557974762d699b4e4b7e02fff2fab8250508
Zaitian Mou <zaitian.mou@gmail.com>
2020-05-24 00:39:57 -0400

    Updates

---
+++ precache-manifest.42a2e9c9027c0b15e8786f8491b9c75e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed258ef73588460624359092ed89161",
+    "url": "/ZaitianMou.github.io/index.html"
+  },
+  {
+    "revision": "931d0063ea1ee77ebfe7",
+    "url": "/ZaitianMou.github.io/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a35f230350debde0589a",
+    "url": "/ZaitianMou.github.io/static/js/2.66740a03.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/ZaitianMou.github.io/static/js/2.66740a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931d0063ea1ee77ebfe7",
+    "url": "/ZaitianMou.github.io/static/js/main.975d7c3a.chunk.js"
+  },
+  {
+    "revision": "fd18fb690139e8d9c070",
+    "url": "/ZaitianMou.github.io/static/js/runtime-main.ecc114f6.js"
+  }
+]);
